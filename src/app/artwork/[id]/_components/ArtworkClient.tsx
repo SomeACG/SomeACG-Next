05fe20f5d0fd697330ed42dd83a0c313309a1e0c
yusixian <cosine_yu@qq.com>
2024-11-09 04:06:16 +0800

    fix: ClientOnly in artwork item

--- src/app/artwork/[id]/_components/ArtworkClient.tsx
+++ src/app/artwork/[id]/_components/ArtworkClient.tsx
@@ -15,41 +15,41 @@ type ArtworkClientProps = {
 
 export default function ArtworkClient({ artwork, originShowUrl, authorUrl, artworkUrl }: ArtworkClientProps) {
   return (
-    <div className="container mx-auto overflow-auto px-4 py-8">
-      <div className="grid grid-cols-1 gap-8 overflow-auto lg:grid-cols-2">
-        <div className="relative aspect-auto">
-          <ClientOnly>
+    <ClientOnly>
+      <div className="container mx-auto overflow-auto px-4 py-8">
+        <div className="grid grid-cols-1 gap-8 overflow-auto lg:grid-cols-2">
+          <div className="relative aspect-auto">
             <PhotoViewer originUrl={originShowUrl} thumbUrl={originShowUrl} title={artwork?.title ?? ''} />
-          </ClientOnly>
-        </div>
-
-        <div className="flex flex-col space-y-6">
-          <h1 className="break-all text-3xl font-bold">{artwork.title}</h1>
-
-          <div className="flex flex-col space-y-4">
-            <div className="flex items-center gap-2">
-              <span className="min-w-16 text-gray-600 dark:text-gray-400">作者：</span>
-              <a href={authorUrl} target="_blank" className="flex items-center gap-2 text-primary hover:underline">
-                {artwork.platform === Platform.Pixiv && <SiPixiv className="text-xl" />}
-                {artwork.platform === Platform.Twitter && <FaSquareXTwitter className="text-xl" />}
-                {artwork.author}
-              </a>
-            </div>
-
-            <div className="flex items-center gap-2">
-              <span className="min-w-16 text-gray-600 dark:text-gray-400">平台：</span>
-              <span>{artwork.platform}</span>
-            </div>
           </div>
-          <a
-            href={artworkUrl}
-            target="_blank"
-            className="inline-block w-fit rounded-lg bg-primary px-6 py-2 text-white hover:bg-primary/90"
-          >
-            在 {artwork.platform} 上查看原图
-          </a>
+
+          <div className="flex flex-col space-y-6">
+            <h1 className="break-all text-3xl font-bold">{artwork.title}</h1>
+
+            <div className="flex flex-col space-y-4">
+              <div className="flex items-center gap-2">
+                <span className="min-w-16 text-gray-600 dark:text-gray-400">作者：</span>
+                <a href={authorUrl} target="_blank" className="flex items-center gap-2 text-primary hover:underline">
+                  {artwork.platform === Platform.Pixiv && <SiPixiv className="text-xl" />}
+                  {artwork.platform === Platform.Twitter && <FaSquareXTwitter className="text-xl" />}
+                  {artwork.author}
+                </a>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <span className="min-w-16 text-gray-600 dark:text-gray-400">平台：</span>
+                <span>{artwork.platform}</span>
+              </div>
+            </div>
+            <a
+              href={artworkUrl}
+              target="_blank"
+              className="inline-block w-fit rounded-lg bg-primary px-6 py-2 text-white hover:bg-primary/90"
+            >
+              在 {artwork.platform} 上查看原图
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </ClientOnly>
   );
 }
