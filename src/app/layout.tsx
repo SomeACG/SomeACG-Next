3f82c4c9433dc9ef9dd8be5a3a49a03d4fe81d5d
yusixian <cosine_yu@qq.com>
2025-01-15 13:02:55 +0800

    feat: add umami statistic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,10 @@ import Root from '@/components/layout/root';
 import { fontVariants } from '@/constants/font';
 import { cn } from '@/lib/utils';
 import Providers from './providers';
+import Script from 'next/script';
 
-import '../styles/globals.css';
 import 'react-photo-view/dist/react-photo-view.css';
+import '../styles/globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="description" content="质量超高的 ACG 图片列表，包好看的！" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <Script defer src="https://stats.cosine.ren/script.js" data-website-id="0d0ceb4b-b171-4831-ad6c-ae49f0672287"></Script>
       </head>
       <body className={cn(`m-0 h-full p-0 font-sans`, ...fontVariants)} suppressHydrationWarning>
         <Providers>
