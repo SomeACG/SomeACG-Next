a84562f6bc46ac828af1265db61ba1c89f09bdc2
yusixian <cosine_yu@qq.com>
2025-02-23 02:20:22 +0800

    chore: update project configuration and PM2 scripts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'cos-pic-db-sync',
-      script: 'python3',
-      args: 'scripts/sync_db.py',
-      autorestart: false, // 执行完成后不自动重启
-      watch: false,
-      env: {
-        NODE_ENV: 'production',
-      },
-    },
-    {
-      name: 'cos-pic-next',
+      name: 'SomeACG-Next',
       script: 'npm',
       args: 'run restart',
       env: {
