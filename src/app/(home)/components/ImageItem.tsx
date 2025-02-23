c55dd09177eea9ce9774044bd7cbd30d38aa0f5f
yusixian <cosine_yu@qq.com>
2025-02-24 01:44:03 +0800

    refactor: improve ImageItem

--- src/app/(home)/components/ImageItem.tsx
+++ src/app/(home)/components/ImageItem.tsx
@@ -11,6 +11,7 @@ import { SiPixiv } from 'react-icons/si';
 import { PhotoView } from 'react-photo-view';
 import { ImageHoverCard } from './ImageHoverCard';
 import { Image } from '@prisma/client';
+import { useIsMobile } from '@/hooks/useIsMobile';
 
 interface ImageItemProps {
   data: Image;
@@ -22,7 +23,7 @@ export function ImageItem({ data, className }: ImageItemProps) {
   const [isHover, setIsHover] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
-
+  const isMobile = useIsMobile();
   const thumbShowUrl = useMemo(() => transformPixivUrl(thumburl ?? ''), [thumburl]);
   const originShowUrl = useMemo(() => transformPixivUrl(rawurl ?? ''), [rawurl]);
   const authorUrl = useMemo(
@@ -68,52 +69,57 @@ export function ImageItem({ data, className }: ImageItemProps) {
           </div>
         </div>
       </PhotoView>
-      {/* 桌面端使用hover卡片 */}
-      <AnimatePresence mode="wait">
-        {isHover && (
-          <div className="block md:hidden">
-            <ImageHoverCard
-              id={id ?? ''}
-              title={title ?? ''}
-              author={author ?? ''}
-              platform={platform as Platform}
-              artworkUrl={artworkUrl ?? ''}
-              authorUrl={authorUrl ?? ''}
-            />
-          </div>
-        )}
-      </AnimatePresence>
-      {/* 移动端显示在底部 */}
-      <div
-        className="bg-primary/20 hidden px-2 py-2 text-white backdrop-blur-lg md:block"
-        onClick={() => router.push(`/artwork/${id}`)}
-      >
-        <h2 className="truncate text-sm/4 font-semibold">{title}</h2>
+      {isMobile ? (
+        <div
+          className="bg-primary/20 hidden px-2 py-2 text-white backdrop-blur-lg md:block"
+          onClick={() => router.push(`/artwork/${id}`)}
+        >
+          {/* 移动端显示在底部 */}
+          <h2 className="truncate text-sm/4 font-semibold">{title}</h2>
 
-        <div className="mt-1.5 flex items-center justify-between gap-1">
-          <a
-            target="_blank"
-            className="flex-center group/author border-primary bg-primary/50 hover:bg-primary/80 h-6 cursor-pointer gap-1.5 rounded-full border px-1.5 hover:text-white"
-            href={authorUrl}
-          >
-            {platform === Platform.Pixiv && <SiPixiv className="size-4 opacity-70 group-hover/author:opacity-100" />}
-            {platform === Platform.Twitter && <FaSquareXTwitter className="size-4 opacity-70 group-hover/author:opacity-100" />}
-            <span className="block max-w-16 truncate text-xs text-white/90 group-hover/author:text-white">{author}</span>
-          </a>
-          <Button
-            variant="ghost"
-            size="xs"
-            className="flex-center border-primary bg-primary/50 hover:bg-primary/80 cursor-pointer gap-0.5 rounded-full border p-0 px-1.5 text-xs text-white hover:text-white"
-            onClick={(e) => {
-              e.stopPropagation();
-              window.open(artworkUrl, '_blank');
-            }}
-          >
-            <FaLink className="size-3.5" />
-            <span className="truncate">原图</span>
-          </Button>
+          <div className="mt-1.5 flex items-center justify-between gap-1">
+            <a
+              target="_blank"
+              className="flex-center group/author border-primary bg-primary/50 hover:bg-primary/80 h-6 cursor-pointer gap-1.5 rounded-full border px-1.5 hover:text-white"
+              href={authorUrl}
+            >
+              {platform === Platform.Pixiv && <SiPixiv className="size-4 opacity-70 group-hover/author:opacity-100" />}
+              {platform === Platform.Twitter && (
+                <FaSquareXTwitter className="size-4 opacity-70 group-hover/author:opacity-100" />
+              )}
+              <span className="block max-w-16 truncate text-xs text-white/90 group-hover/author:text-white">{author}</span>
+            </a>
+            <Button
+              variant="ghost"
+              size="xs"
+              className="flex-center border-primary bg-primary/50 hover:bg-primary/80 cursor-pointer gap-0.5 rounded-full border p-0 px-1.5 text-xs text-white hover:text-white"
+              onClick={(e) => {
+                e.stopPropagation();
+                window.open(artworkUrl, '_blank');
+              }}
+            >
+              <FaLink className="size-3.5" />
+              <span className="truncate">原图</span>
+            </Button>
+          </div>
         </div>
-      </div>
+      ) : (
+        <AnimatePresence mode="wait">
+          {/* 桌面端使用 hover 卡片 */}
+          {isHover && (
+            <div className="block md:hidden">
+              <ImageHoverCard
+                id={id ?? ''}
+                title={title ?? ''}
+                author={author ?? ''}
+                platform={platform as Platform}
+                artworkUrl={artworkUrl ?? ''}
+                authorUrl={authorUrl ?? ''}
+              />
+            </div>
+          )}
+        </AnimatePresence>
+      )}
     </motion.div>
   );
 }
